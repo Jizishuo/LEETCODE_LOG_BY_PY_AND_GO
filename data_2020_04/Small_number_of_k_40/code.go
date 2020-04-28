fe8c1b82e5e503f1fb9cd4d173e5f14b9210694b
Jizishuo <jizishuo@qq.com>
2020-04-28 15:31:49 +0800

    0428

--- data_2020_04/Small_number_of_k_40/code.go
+++ data_2020_04/Small_number_of_k_40/code.go
@@ -1,9 +1,55 @@
 package Small_number_of_k_40
 
-import "sort"
+
 
 func getLeastNumbers(arr []int, k int) []int {
-	sort.Ints(arr)
-	return arr[:k]
+	//sort.Ints(arr)
+	//return arr[:k]
+	if len(arr) == 0 {
+		return arr
+	}
+	left := 0
+	right := len(arr) -1
+	index := partation(arr, left, right)
+	for index != k-1 {
+		if index>k-1 {
+			right = index-1
+			index = partation(arr, left, right)
+		} else {
+			left = index +1
+			index = partation(arr, left, right)
+		}
+	}
+	result := []int{}
+	for i :=0; i<k;i++ {
+		result = append(result, arr[i])
+	}
+	return result
+}
 
+
+func partation(num []int, left, right int) int {
+	if left == right {
+		return right
+	}
+	if left <right {
+		base := num[left]
+		l := left
+		r := right
+		for {
+			for num[r] >=base && l<r {
+				r--
+			}
+			for num[l] <=base && l<r {
+				l++
+			}
+			if l >= r {
+				break
+			}
+			num[l], num[r] = num[r], num[l]
+		}
+		num[left] = num[l]
+		num[l] = base
+	}
+	return -1
 }
