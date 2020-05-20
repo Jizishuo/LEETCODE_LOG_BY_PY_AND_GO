7843bacc463e3bbcd191356f55c46b5c7cb15673
Jizishuo <jizishuo@qq.com>
2020-05-20 10:16:35 +0800

    Find in a 2D array

---
+++ data_2020_05/Find_in_a_2D_array_04/code.py
@@ -0,0 +1,8 @@
+class Solution:
+    def findNumberIn2DArray(self, matrix: List[List[int]], target: int) -> bool:
+        i, j = len(matrix)-1, 0
+        while i>=0 and j < len(matrix[0]):
+            if matrix[i][j] >target:i-=1
+            elif matrix[i][j] < target:j+=1
+            else:return True
+        return False
