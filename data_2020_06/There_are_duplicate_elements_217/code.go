a9e6b132383fd0dec6ef83fa9a14b0f60b96588e
Jizishuo <jizishuo@qq.com>
2020-06-06 09:17:26 +0800

    0606

--- data_2020_06/There_are_duplicate_elements_217/code.go
+++ data_2020_06/There_are_duplicate_elements_217/code.go
@@ -19,9 +19,4 @@ func containsDuplicate(nums []int) bool {
 		m[v]= struct{}{}
 	}
 	return false
-
-	作者：ba-xiang
-	链接：https://leetcode-cn.com/problems/contains-duplicate/solution/go-hash-by-ba-xiang-4/
-	来源：力扣（LeetCode）
-	著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
 }
