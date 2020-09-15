895790b8314abd484c24aa481e9164d961846d24
jizishuo <jizishuo@qq.com>
2020-09-15 22:10:45 +0800

    1290. 二进制链表转整数

---
+++ data_2020_09/Binary_list_to_integer_1290/code.go
@@ -0,0 +1,15 @@
+package Binary_list_to_integer_1290
+
+type ListNode struct {
+    Val int
+    Next *ListNode
+}
+
+func getDecimalValue(head *ListNode) int {
+	sum :=0
+	for head != nil {
+		sum = sum*2 + head.Val
+		head = head.Next
+	}
+	return sum
+}
---
+++ data_2020_09/Binary_list_to_integer_1290/code.py
@@ -0,0 +1,12 @@
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution:
+    def getDecimalValue(self, head: ListNode) -> int:
+        res = '0b'
+        while head:
+            res += str(head.val)
+            head = head.next
+        return int(res, 2)
