219360b4132d745fc8a8e434ce4b7c7e5389d62f
jizishuo <jizishuo@qq.com>
2020-11-18 22:17:28 +0800

    182. 查找重复的电子邮箱

---
+++ data_2020_11/Look_for_duplicate_email_addresses_182/code.sql
@@ -0,0 +1,11 @@
+-- select Email from
+-- (
+--     select Email, count(Email) as num
+--     from Person
+--     group by Email
+-- ) as statistic
+-- where num >1
+select Email
+from Person
+group by Email
+having count(Email) > 1;
